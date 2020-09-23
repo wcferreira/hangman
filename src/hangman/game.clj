dafb590d5427db376b9b106f0c1cb2b9e795f36f
Wagner Ferreira <wcarvalho.ferreira@gmail.com>
2020-09-23 15:11:13 -0300

    fix: add docstring

--- src/hangman/game.clj
+++ src/hangman/game.clj
@@ -4,16 +4,19 @@
 (def correct_chars (atom []))
 
 (defn display-welcome-message
+  "Displays game welcome message"
   []
   (println "=========================================")
   (println "====== Welcome to the Hangman Game ======")
   (println "========================================="))
 
 (defn initialize-correct-guesses
+  "Initialize with _ (underscore) a vector that will hold the correct guesses"
   [secret-word]
   (reset! correct_chars (into [] (repeat (count secret-word) "_"))))
 
 (defn play
+  "Entry point"
   []
   (display-welcome-message)
   (initialize-correct-guesses secret))
