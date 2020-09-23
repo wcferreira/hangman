ddfd0dba2d39bd908442278aa74390495972b460
Wagner Ferreira <wcarvalho.ferreira@gmail.com>
2020-09-23 14:58:10 -0300

    feat: creates function to display welcome message

--- src/hangman/game.clj
+++ src/hangman/game.clj
@@ -1,1 +1,16 @@
 (ns hangman.game)
+
+(def secret "clojure")
+(def correct_chars (atom (into [] (repeat (count secret) "_"))))
+
+(defn display-welcome-message
+  []
+  (println "=========================================")
+  (println "====== Welcome to the Hangman Game ======")
+  (println "========================================="))
+
+(defn play
+  []
+  (display-welcome-message)
+  (println @correct_chars)
+  )
