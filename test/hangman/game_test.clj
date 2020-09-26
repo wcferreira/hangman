7b9f30221c89d36ce353e9d15505b28d4e2236ec
Wagner Ferreira <wcarvalho.ferreira@gmail.com>
2020-09-26 18:20:11 -0300

    test: creates tests for the get-secret-word function

--- test/hangman/game_test.clj
+++ test/hangman/game_test.clj
@@ -1,2 +1,16 @@
 (ns hangman.game-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [hangman.game :as hg]))
+
+
+(deftest test-get-secret-word
+  (testing "It should return a secret word when list is not empty"
+    (let [fruits ["banana" "guava" "strawberry" "apple" "avocado"]
+          secret-word (hg/get-secret-word fruits)]
+      (is (true? (contains? (set fruits) secret-word)))))
+  (testing "It should return nil when list is empty"
+    (let [fruits []
+          secret-word (hg/get-secret-word fruits)]
+      (is (nil? secret-word)))))
+
+
