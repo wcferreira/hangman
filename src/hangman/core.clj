05f8462056d85a9d34a439b1c5596b6b1c17b7c7
Wagner Ferreira <wcarvalho.ferreira@gmail.com>
2020-09-27 14:30:42 -0300

    feat: add some game initializations functions in main

--- src/hangman/core.clj
+++ src/hangman/core.clj
@@ -1,7 +1,13 @@
 (ns hangman.core
   (:gen-class)
-  (:require [hangman.game :as hg]))
+  (:require [hangman.game :as hg]
+            [hangman.drawings :as hd]))
 
 (defn -main
+  "Entry point"
   []
-  (hg/play))
+  (let [words (hg/read-words-from-file)
+        secret-word (hg/get-secret-word words)]
+  (hd/display-welcome-message)
+  (hg/initialize-correct-guesses secret-word hg/correct_guesses)
+  (hg/play secret-word hg/correct_guesses)))
