8dd0c7bc1cb147b3ab3e170330bbf45cc8ff820a
Wagner Ferreira <wcarvalho.ferreira@gmail.com>
2020-09-27 15:36:40 -0300

    fix: convert word read from file to lower case

--- src/hangman/core.clj
+++ src/hangman/core.clj
@@ -1,13 +1,15 @@
 (ns hangman.core
   (:gen-class)
   (:require [hangman.game :as hg]
-            [hangman.drawings :as hd]))
+            [hangman.drawings :as hd]
+            [clojure.string :as s]))
 
 (defn -main
   "Entry point"
   []
   (let [words (hg/read-words-from-file)
-        secret-word (hg/get-secret-word words)]
+        word (hg/get-secret-word words)
+        secret-word (s/lower-case word)]
   (hd/display-welcome-message)
   (hg/initialize-correct-guesses secret-word hg/correct_guesses)
   (hg/play secret-word hg/correct_guesses)))
