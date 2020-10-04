4684a8e3210a22c211c0ed204ac21e74ca89dd87
Wagner Ferreira <wcarvalho.ferreira@gmail.com>
2020-10-04 11:50:22 -0300

    test: validate read-file function

--- test/hangman/game_test.clj
+++ test/hangman/game_test.clj
@@ -2,21 +2,34 @@
   (:require [clojure.test :refer :all]
             [hangman.game :as hg]
             [schema.core :as s])
-  (:import (clojure.lang ExceptionInfo)))
+  (:import (clojure.lang ExceptionInfo)
+           (java.io FileNotFoundException)))
 
 (s/set-fn-validation! true)
 
 (deftest test-get-secret-word
-  (testing "It should return AssertionError when passed in an empty list"
+  (testing "It should throw AssertionError when passed in an empty list"
     (is (thrown? AssertionError (hg/get-secret-word []))))
 
-  (testing "It should return ExceptionInfo when a vector of integers is passed in"
+  (testing "It should throw ExceptionInfo when a vector of integers is passed in"
     (is (thrown? ExceptionInfo (hg/get-secret-word [22 5 67]))))
 
   (testing "It should return a word when a vector of strings is passed in"
     (let [fruits ["banana" "guava" "strawberry" "apple" "avocado"]
           secret-word (hg/get-secret-word fruits)]
       (is (true? (contains? (set fruits) secret-word))))))
+
+(deftest test-read-file
+  (testing "It should throw FileNotFoundException when an invalid file path is passed in"
+    (is (thrown? FileNotFoundException (hg/read-file "anything.txt"))))
+
+  (testing "It should return a string when a valid file path is passed in"
+    (let [data (hg/read-file "resources/fruits.txt")]
+      (println )
+      (is (string? data)))))
+
+
+
 
 
 
