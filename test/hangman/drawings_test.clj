2486b15f91ee05d125dc8ca96322b68fe83ce7f8
Wagner Ferreira <wcarvalho.ferreira@gmail.com>
2020-10-06 15:20:13 -0300

    test: validate draw-hangman function

--- test/hangman/drawings_test.clj
+++ test/hangman/drawings_test.clj
@@ -1,6 +1,10 @@
 (ns hangman.drawings-test
   (:require [clojure.test :refer :all]
-            [hangman.drawings :as hd]))
+            [hangman.drawings :as hd]
+            [schema.core :as s])
+  (:import (clojure.lang ExceptionInfo)))
+
+(s/set-fn-validation! true)
 
 (deftest test-display-welcome-message
   (let [expected (str "=========================================\n"
@@ -84,3 +88,35 @@
                       " |            \n"
                       "_|___         \n")]
     (is (= expected (hd/draw-left-leg)))))
+
+(deftest test-draw-hangman
+  (testing "It should throw ClassCastException when a parameter different from integer is passed in"
+    (is (thrown? ExceptionInfo (hd/draw-hangman "234")))
+    (is (thrown? ExceptionInfo (hd/draw-hangman 33.4)))
+    (is (thrown? ExceptionInfo (hd/draw-hangman []))))
+
+  (testing "It should throw AssertionError when a number less than or equal to zero or greater than 7 is passed in"
+    (is (thrown? AssertionError (hd/draw-hangman 0)))
+    (is (thrown? AssertionError (hd/draw-hangman -1)))
+    (is (thrown? AssertionError (hd/draw-hangman 8))))
+
+  (testing "It should return the head when 1 is passed in"
+    (is (= (hd/draw-head) (hd/draw-hangman 1))))
+
+  (testing "It should return the head and right arm when 2 is passed in"
+    (is (= (hd/draw-right-arm) (hd/draw-hangman 2))))
+
+  (testing "It should return head, right arm and chest when 3 is passed in"
+    (is (= (hd/draw-chest) (hd/draw-hangman 3))))
+
+  (testing "It should return head, right arm, chest and left arm when 4 is passed in"
+    (is (= (hd/draw-left-arm) (hd/draw-hangman 4))))
+
+  (testing "It should return head, right arm, chest, left arm and thorax when 5 is passed in"
+    (is (= (hd/draw-thorax) (hd/draw-hangman 5))))
+
+  (testing "It should return head, right arm, chest, left arm, thorax and right leg when 6 is passed in"
+    (is (= (hd/draw-right-leg) (hd/draw-hangman 6))))
+
+  (testing "It should return head, right arm, chest, left arm, thorax, right leg and left leg when 7 is passed in"
+    (is (= (hd/draw-left-leg) (hd/draw-hangman 7)))))
